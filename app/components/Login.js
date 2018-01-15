cdc91ae5d76fa05f833e8b8d220bbeb2909c703e
vitorcx <vitorcx010@gmail.com>
2018-01-15 19:23:47 -0200

    Adicionando imagens dos icones da logo

--- app/components/Login.js
+++ app/components/Login.js
@@ -3,14 +3,35 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 import { Navigator } from 'react-native-deprecated-custom-components';
 
 export default class  Login extends Component {
   render() {
     return(
-      <Text>Login</Text>
+      <View style = {styles.container}>
+        <View style = {styles.logoContainer}>
+          <Image
+            source = {require('../img/xhdpi/ic_launcher_foreground.png')}
+          />
+        </View>
+        <View style = {styles.formContainer}>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1565C0",//material blue 800
+  },
+  logoContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center',
+  },
+});
